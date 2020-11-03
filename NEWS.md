70d1935460ca23c0e116e7c79cab3056a0356503
dieghernan <diego.hernangomezherrero@gmail.com>
2020-11-03 15:10:17 +0000

    Add new functions

--- NEWS.md
+++ NEWS.md
@@ -2,11 +2,23 @@
 
 - Remove `colorspace` as dependency.
 - Bump R minimal version to 3.6.0.
+- Change order on parameters for `gisco_get()` functions.
+- Rewriting of internal functions and utils.
+- Rewriting of `giscoR::gisco_db`.
+- Functions added:
+  - `gisco_bulk_download()`
+  - `gisco_check_access()`
+  - `gisco_get_airports()`
+  - `gisco_get_grid()`
+  - `gisco_get_healthcare()`
+  - `gisco_get_ports()`
+  - `gisco_get_units()`
+- Now `gisco_get_countries()` and `gisco_get_nuts()` uses `gisco_get_units()` for individual files, making the call much faster.
 
 
 # giscoR 0.1.1
 
-- Added `tgs00026` dataset.
+- Added `giscoR::tgs00026` dataset.
 - Remove `eurostat` dependency.
 
 
---
+++ R/gisco_get_healthcare.R
@@ -0,0 +1,60 @@
+#' @title Get the healthcare services in Europe.
+#' @description The dataset contains information on main healthcare services considered to be 'hospitals' by Member States.
+#' @return A \code{POINT} object.
+#' @author dieghernan, \url{https://github.com/dieghernan/}
+#' @source \href{https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/healthcare-services}{GISCO Healthcare services}
+#' @param cache,update_cache,cache_dir,verbose,country See \link{gisco_get}
+#' @details Files are distributed on EPSG:4326. \href{https://gisco-services.ec.europa.eu/pub/healthcare/metadata.pdf}{Link to metadata}
+#' @examples
+#' library(sf)
+#'
+#' if (gisco_check_access()) {
+#'   HospitalBENELUX <- gisco_get_healthcare(country = c("BE", "NL", "LU"))
+#'   BENELUX <- gisco_get_countries(country = c("BE", "NL", "LU"))
+#'   plot(st_geometry(BENELUX))
+#'   plot(
+#'     st_geometry(HospitalBENELUX),
+#'     pch = 20,
+#'     col = "steelblue1",
+#'     add = TRUE
+#'   )
+#'   title(main = "Hospitals in Benelux",
+#'         sub = gisco_attributions(),
+#'         line = 1)
+#' }
+#' @seealso \link{gisco_get}
+#' @export
+gisco_get_healthcare <- function(cache = TRUE,
+                                 update_cache = FALSE,
+                                 cache_dir = NULL,
+                                 verbose = FALSE,
+                                 country = NULL) {
+  # Given vars
+  epsg = "4326"
+  ext <- "geojson"
+
+  geturl <-
+    list(api.url = "https://gisco-services.ec.europa.eu/pub/healthcare/geojson/all.geojson",
+         namefile = "gisco_healthcare.geojson")
+
+
+  if (cache) {
+    # Guess source to load
+    namefileload <-
+      gsc_api_cache(geturl$api.url,
+                    geturl$namefile,
+                    cache_dir,
+                    update_cache,
+                    verbose)
+  } else {
+    namefileload <- geturl$api.url
+  }
+  data.sf <-
+    gsc_api_load(namefileload, epsg, ext, cache, verbose)
+
+  if (!is.null(country) & "cc" %in% names(data.sf)) {
+    country <- gsc_helper_countrynames(country, "eurostat")
+    data.sf <- data.sf[data.sf$cc %in% country, ]
+  }
+  return(data.sf)
+}
