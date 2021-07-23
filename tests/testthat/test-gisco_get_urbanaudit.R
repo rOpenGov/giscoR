e875b66cc6b49a4b2dae688f577caa07ef2e7af4
dieghernan <diego.hernangomezherrero@gmail.com>
2021-07-23 14:12:07 +0000

    Update tests

---
+++ tests/testthat/test-gisco_get_urbanaudit.R
@@ -0,0 +1,7 @@
+test_that("Urban Audit offline", {
+  expect_error(gisco_get_urban_audit(year = "1999"))
+  expect_error(gisco_get_urban_audit(epsg = "9999"))
+  expect_error(gisco_get_urban_audit(level = "9999"))
+  expect_error(gisco_get_urban_audit(spatialtype = "BN"))
+  expect_error(gisco_get_urban_audit(year = 2001))
+})
