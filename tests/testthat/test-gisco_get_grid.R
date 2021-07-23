eb7afde66876107f364ecc83da691134b12f7b44
dieghernan <diego.hernangomezherrero@gmail.com>
2021-07-23 15:37:11 +0200

    Fix some tests

--- tests/testthat/test-gisco_get_grid.R
+++ tests/testthat/test-gisco_get_grid.R
@@ -5,6 +5,10 @@ test_that("Grid offline", {
 })
 
 test_that("Grids online", {
+  skip_if_not(
+    gisco_check_access(),
+    "Skipping... GISCO not reachable."
+  )
   expect_silent(gisco_get_grid(100))
   expect_message(gisco_get_grid(100, verbose = TRUE))
   expect_message(gisco_get_grid(100, spatialtype = "POINT", verbose = TRUE))
