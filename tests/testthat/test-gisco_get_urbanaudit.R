8bae561244eba03ff3d459195815d6f4598d81f7
dieghernan <diego.hernangomezherrero@gmail.com>
2021-07-23 14:17:33 +0000

    Add online tests

--- tests/testthat/test-gisco_get_urbanaudit.R
+++ tests/testthat/test-gisco_get_urbanaudit.R
@@ -5,3 +5,62 @@ test_that("Urban Audit offline", {
   expect_error(gisco_get_urban_audit(spatialtype = "BN"))
   expect_error(gisco_get_urban_audit(year = 2001))
 })
+
+test_that("Urban Audit online", {
+  skip_if_not(
+    gisco_check_access(),
+    "Skipping... GISCO not reachable."
+  )
+
+  expect_silent(gisco_get_urban_audit(level = "GREATER_CITIES"))
+  expect_silent(gisco_get_urban_audit(
+    spatialtype = "LB",
+    level = "GREATER_CITIES"
+  ))
+
+  expect_silent(
+    gisco_get_urban_audit(
+      level = "GREATER_CITIES",
+      spatialtype = "LB",
+      epsg = 3857,
+      country = c("ITA", "POL")
+    )
+  )
+
+  expect_silent(
+    gisco_get_urban_audit(
+      year = 2014,
+      spatialtype = "LB",
+      level = "GREATER_CITIES",
+      epsg = 3857,
+      country = c("ITA", "POL")
+    )
+  )
+
+  expect_silent(gisco_get_urban_audit(
+    year = 2018,
+    epsg = 3857,
+    level = "GREATER_CITIES",
+    country = c("ITA", "POL")
+  ))
+
+  expect_message(
+    gisco_get_urban_audit(
+      year = 2018,
+      spatialtype = "LB",
+      epsg = 3857,
+      country = c("ITA", "POL"),
+      verbose = TRUE
+    )
+  )
+
+  expect_silent(
+    gisco_get_urban_audit(
+      year = 2020,
+      spatialtype = "LB",
+      level = "GREATER_CITIES",
+      epsg = 3857,
+      country = c("ITA", "POL")
+    )
+  )
+})
