cf62b48ce5ff8ee164b6cc7d1625097a38ba4ff5
Diego H. <dieghernan@users.noreply.github.com>
2025-12-02 02:11:29 +0100

    Fix test

--- tests/testthat/test-docs.R
+++ tests/testthat/test-docs.R
@@ -3,7 +3,7 @@ test_that("Check docs", {
   skip_if_gisco_offline()
   # Load databases now
   db <- gisco_get_latest_db()
-  db <- gisco_get_latest_db_units()
+
   expect_snapshot(db_values("communes", "year"))
   expect_snapshot(db_values("communes", "year", decreasing = TRUE))
   expect_snapshot(db_values("communes", "ext"))
